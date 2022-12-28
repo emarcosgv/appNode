412d1a081bbd34d884d6134428007264630d1d7e
Emmanuel Marcos Gabriel Villalba <evillalba@frba.utn.edu.ar>
2022-12-28 03:27:46 -0300

    primera vista handlebars

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,3 +1,20 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import * as path from "path";
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
+
+
 const controller = {};
 
 
